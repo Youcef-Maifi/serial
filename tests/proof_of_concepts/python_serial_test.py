535abfbc852b16f7d8473a9e9cbd8bcb614ecccd
Youcef-Maifi <yovoy@outlook.sa>
2023-12-03 14:26:54 +0100

    usb

--- tests/proof_of_concepts/python_serial_test.py
+++ tests/proof_of_concepts/python_serial_test.py
@@ -3,7 +3,7 @@
 import serial, sys
 
 if len(sys.argv) != 2:
-    print "python: Usage_serial_test <port name like: /dev/ttyUSB0>"
+    print "python: Usage_serial_test <port name like: /dev/serial/by-id/usb-1a86_USB_Serial-if00-port0>"
     sys.exit(1)
 
 sio = serial.Serial(sys.argv[1], 115200)
